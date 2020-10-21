94f412dbd351a6984368379989dc219ed0d40c19
aditya <imaditya1106@gmail.com>
2020-10-21 11:12:37 +0530

    first commit

---
+++ track-server/src/middlewares/requireAuth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).send({ error: "you must ne logged in" });
+  }
+  const token = authorization.replace("Bearer ", "");
+  jwt.verify(token, "MY_SECRET_KEY", async (err, payload) => {
+    if (err) {
+      return res.status(401).send({ error: "you must be logged in." });
+    }
+    const { userID } = payload;
+    const user = await User.findById(userID);
+    req.user = user;
+    next();
+  });
+};
